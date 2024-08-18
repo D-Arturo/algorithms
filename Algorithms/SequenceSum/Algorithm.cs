add05f8509924f997b714897227a8a15dbf4513c
D-Arturo <arturomf88@gmail.com>
2024-08-18 14:45:32 +0200

    two exercises

---
+++ Algorithms/SequenceSum/Algorithm.cs
@@ -0,0 +1,59 @@
+﻿namespace Algorithms.SequenceSum;
+
+public class Algorithm
+{
+    public static int SequenceCalculator(int start, int end, int step)
+    {
+        var accumulated = 0;
+        for (var i = start; i <= end; i += step)
+        {
+            accumulated += i;
+        }
+
+        return accumulated;
+    }
+
+    public static int QueueTime(int[] customers, int queuesAvailable)
+    {
+        if (queuesAvailable == 1) return customers.Length == 0 ? 0 : customers.Sum();
+        
+        var queue1 = new List<int>();
+        var queue2 = new List<int>();
+        
+        foreach (var customer in customers)
+        {
+            if (queue1.Count == 0)
+            {
+                queue1.Add(customer);
+                continue;
+            }
+
+            if (queue2.Count == 0)
+            {
+                queue2.Add(customer);
+                continue;
+            }
+
+            AssignToQueue(customer);
+        }
+
+        return CalculateWaitingTime();
+
+        void AssignToQueue(int customer)
+        {
+            if (queue1.Sum() > queue2.Sum())
+            {
+                queue2.Add(customer);
+            }
+            else
+            {
+                queue1.Add(customer);
+            }
+        }
+
+        int CalculateWaitingTime()
+        {
+            return queue1.Sum() >= queue2.Sum() ? queue1.Sum() : queue2.Sum();
+        }
+    }
+}
---
+++ AlgorithmsTests/SequenceSum/AlgorithmShould.cs
@@ -0,0 +1,29 @@
+﻿using Algorithms.SequenceSum;
+
+namespace AlgorithmsTests.SequenceSum;
+
+public class AlgorithmShould
+{
+    [Theory]
+    [InlineData(1, 1, 1, 1)]
+    [InlineData(2, 2, 2, 2)]
+    [InlineData(2,4,2, 6)]
+    [InlineData(2,6,2, 12)]
+    [InlineData(1,5,1, 15)]
+    [InlineData(1,5,3, 5)]
+    [InlineData(5,1,3, 0)]
+    public void ReturnCalculatedSequenceValue(int start, int end, int step, int expected)
+    {
+        Assert.Equal(expected, Algorithm.SequenceCalculator(start, end, step));
+    }
+    
+    [Theory]
+    [InlineData(new int []{}, 1, 0)]
+    [InlineData(new int[] { 1, 2, 3, 4 }, 1, 10)]
+    [InlineData(new int[] { 1, 2, 3}, 2, 4)]
+    [InlineData(new int[] { 2, 2, 3, 3, 4, 4 }, 2, 9)]
+    public void ReturnCalculatedSupermarketQueueValue(int[] customers, int queuesAvailable, int expectedTime)
+    {
+        Assert.Equal(expectedTime, Algorithm.QueueTime(customers, queuesAvailable));
+    }
+}
